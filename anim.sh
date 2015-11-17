7fca313978ab11f9b2552c8f8c98299a567141f2
Alexey Fomenko <alexey.fomenko@gmail.com>
2015-11-17 23:05:09 +0200

    store pngs, recreate only when new ones appear

--- anim.sh
+++ anim.sh
@@ -3,20 +3,24 @@
 [ -d plot ] || mkdir plot
 [ -d png ] || mkdir png
 
-echo "cleaning graphics"
-rm png/*
-rm anim.gif
+output_file=anim.gif
+recreate=0
 
 echo "creating new png files"
 for i in `ls plot/*c.data | sed 's/plot\///'`; do
+    [ -f png/$i.png ] && continue;
 	echo "starting $i";
+    recreate=1
 	avg=`echo $i | sed 's/_c/_avg/'`;
 	prim=`echo $i | sed 's/_c/_p/'`;
 	echo "avg=$avg";
 	gnuplot -e "sourcefn=\"plot/$i\"; avgfn=\"plot/$avg\"; primfn=\"plot/$prim\"; targetfn=\"png/$i.png\"" plot.gnuplot;
 done;
 
-echo "creating new animated gif 'anim.gif'"
-convert -delay 20 png/* anim.gif
+if [ "x$recreate" == "x1" ]; then
+    [ -f $output_file ] && rm $output_file
+    echo "creating new animated gif: $output_file"
+#    convert -delay 20 png/* $output_file
+fi
 
 echo "Done"
